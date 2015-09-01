5ca38e5d378621d5cbbe82191227633af2962a39
Tomi Korpipää <tomi.korpipaa@theqtcompany.com>
2015-09-01 11:19:18 +0300

    Removed unnecessary Enterprise version of example
    Change-Id: If8152a586fc0e0c704f6494a80a33aa5d73f455a
    Task-number: QTBUG-46917
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>

--- examples/canvas3d/canvas3d/jsonmodels/main.cpp
+++ examples/canvas3d/canvas3d/jsonmodels/main.cpp
@@ -34,8 +34,6 @@
 **
 ****************************************************************************/
 
-#define NO_ENTERPRISE
-
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
@@ -44,11 +42,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-#ifdef NO_ENTERPRISE
-    engine.load(QUrl(QStringLiteral("qrc:/jsonmodelsbasic.qml")));
-#else
     engine.load(QUrl(QStringLiteral("qrc:/jsonmodels.qml")));
-#endif
 
     return app.exec();
 }
