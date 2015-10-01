9d1866e334e2de0f9fe82ef24a3d1bdc38f7b497
Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>
2015-10-01 16:05:56 +0300

    Fix crash when using zero size canvas
    Now the canvas always creates at least size 1x1 frame buffer.
    
    Change-Id: Ie1a3bfc23aa66a8b42fc18b1a8cf46f6ddca8407
    Task-number: QTBUG-48544
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@theqtcompany.com>
    Reviewed-by: Pasi Keränen <pasi.keranen@digia.com>

--- src/imports/qtcanvas3d/canvas3d.cpp
+++ src/imports/qtcanvas3d/canvas3d.cpp
@@ -160,7 +160,6 @@ void Canvas::setWidth(int width)
                                              << maxWidth;
         newWidth = maxWidth;
     }
-
     QQuickItem::setWidth(qreal(newWidth));
 }
 
@@ -454,7 +453,6 @@ void Canvas::setPixelSize(QSize pixelSize)
     qCDebug(canvas3drendering).nospace() << "Canvas3D::" << __FUNCTION__
                                          << "(pixelSize:" << pixelSize
                                          << ")";
-
     if (pixelSize.width() > m_maxSize.width()) {
         qCDebug(canvas3drendering).nospace() << "Canvas3D::" << __FUNCTION__
                                              << "():"
@@ -470,6 +468,11 @@ void Canvas::setPixelSize(QSize pixelSize)
                                              << m_maxSize.height();
         pixelSize.setHeight(m_maxSize.height());
     }
+
+    if (pixelSize.width() <= 0)
+        pixelSize.setWidth(1);
+    if (pixelSize.height() <= 0)
+        pixelSize.setHeight(1);
 
     if (m_fboSize == pixelSize)
         return;
@@ -583,6 +586,10 @@ bool Canvas::firstSync()
     if (!m_renderer->qtContextResolved()) {
         m_firstSync = false;
         QSize initializedSize = boundingRect().size().toSize();
+        if (initializedSize.width() <= 0)
+            initializedSize.setWidth(1);
+        if (initializedSize.height() <= 0)
+            initializedSize.setHeight(1);
         m_renderer->resolveQtContext(window(), initializedSize, m_renderTarget);
         m_isOpenGLES2 = m_renderer->isOpenGLES2();
 
